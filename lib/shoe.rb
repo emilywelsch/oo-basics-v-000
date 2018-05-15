c5836ca34c68a684f0ddd0ded8516ca941b5eb0d
Emily Welsch <emwelsch@gmail.com>
2018-05-15 21:05:57 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,14 @@
 # learn spec/02_shoe_spec.rb
 class Shoe
-
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  
   def initialize(brand)
     @brand = brand
   end
 
+  def cobble
+    puts "the shoe has been repaired"
+  end
   
 end
